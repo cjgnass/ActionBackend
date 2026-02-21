7464fc706365ae901577880ecb62c13850ddac4a
Colin Gnassounou <cjgnass@gmail.com>
2026-02-21 19:15:02 +0000

    websocket tests

--- test_main.py
+++ test_main.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from fastapi.testclient import TestClient
 
 from main import app
@@ -9,3 +11,10 @@ def test_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello!"}
+
+
+def test_ws():
+    with client.websocket_connect("/ws") as ws:
+        ws.send_text("hello")
+        response == ws.receive_text()
+        assert response == "Echo: hello"
